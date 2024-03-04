004162a59550bf05c39fcd1e79a7ab2be5a536d3
Alejandro <95312462+AGMASO@users.noreply.github.com>
2024-03-04 17:30:55 +0100

    Update coderscomp.tsx

--- tradify-front/app/ui/coderscomp.tsx
+++ tradify-front/app/ui/coderscomp.tsx
@@ -4,15 +4,15 @@ import { FaGithub } from "react-icons/fa";
 
 export default function CodersComp() {
   return (
-    <div className="flex flex-col justify-center text-white bg-gradient-to-bl from-gradient-1-start to-gradient-4-end h-full w-full rounded-md pt-4">
-      <div className="flex justify-center">
-        <p className="text-3xl">Creator</p>
+    <div className='flex flex-col justify-center text-white bg-gradient-to-bl from-gradient-1-start to-gradient-4-end h-full w-full rounded-md pt-4'>
+      <div className='flex justify-center'>
+        <p className='text-3xl'>Creator</p>
       </div>
-      <div className="flex justify-around mt-6">
+      <div className='flex justify-around mt-6'>
         <Link
-          className="flex justify-between items-center w-24"
-          href="https://github.com/agmaso"
-          target="_blank"
+          className='flex justify-between items-center w-24'
+          href='https://github.com/agmaso'
+          target='_blank'
         >
           Agmaso <FaGithub />
         </Link>
