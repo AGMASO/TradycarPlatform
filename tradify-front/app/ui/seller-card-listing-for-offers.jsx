1cc4719feb2f620b4b02649c116893230e0a5a79
Alejandro <95312462+AGMASO@users.noreply.github.com>
2024-03-04 17:34:29 +0100

    Update seller-card-listing-for-offers.jsx

--- tradify-front/app/ui/seller-card-listing-for-offers.jsx
+++ tradify-front/app/ui/seller-card-listing-for-offers.jsx
@@ -1,27 +1,35 @@
 import React from "react";
 import { useState } from "react";
-import {Card, CardHeader, CardBody, Image, CardFooter} from "@nextui-org/react";
-import {Button} from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+  CardFooter,
+} from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import Link from "next/link";
 
-import createOffer from '../lib/scripts/createOffer';
+import createOffer from "../lib/scripts/createOffer";
 
-
-export default function SellerCardListingOffer({id,
+export default function SellerCardListingOffer({
+  id,
   key,
   seller,
   carListingIndex,
   nftAddress,
   price,
-  tokenId , image ,
-  name, model, odometer, repairs,
-  repairsDate }) {
-
-
-  
+  tokenId,
+  image,
+  name,
+  model,
+  odometer,
+  repairs,
+  repairsDate,
+}) {
   const [isOpen, setIsOpen] = useState(false);
   const [formData, setFormData] = useState({
-    price: ""
+    price: "",
   });
 
   const handleChange = (e) => {
@@ -34,15 +42,11 @@ export default function SellerCardListingOffer({id,
     e.preventDefault();
 
     try {
+      console.log(nftAddress, tokenId, formData.price);
 
-      console.log(nftAddress,tokenId, formData.price);
-
-       
-
-      await createOffer(nftAddress, formData.price, tokenId, );
+      await createOffer(nftAddress, formData.price, tokenId);
 
       console.log("Success to create Offer");
-      
     } catch (error) {
       console.error("Error submitting form:", error);
     }
@@ -53,47 +57,59 @@ export default function SellerCardListingOffer({id,
   const handleCloseModal = () => {
     // Reset the form data when the modal is closed
     setFormData({
-     
-      price: ""
+      price: "",
     });
     setIsOpen(false);
   };
 
   return (
-    <Card className="py-4 min-h-[70vh]" key={carListingIndex}>
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-      <h4 className="font-bold text-large">{name}</h4>
-      <small className="text-default-500">{model}</small>
-      <p className="text-tiny uppercase font-bold text-indigo-600 pt-[5px] text-end"><span className=" text-[8px]">Owned by  </span> { seller.slice(0,5) + "..." + seller.slice(-5)}</p>
-      <p className="text-tiny uppercase font-bold text-indigo-600 pt-[5px] text-end text-[8px]">Token Id  {tokenId}</p>
-
+    <Card className='py-4 min-h-[70vh]' key={carListingIndex}>
+      <CardHeader className='pb-0 pt-2 px-4 flex-col items-start'>
+        <h4 className='font-bold text-large'>{name}</h4>
+        <small className='text-default-500'>{model}</small>
+        <p className='text-tiny uppercase font-bold text-indigo-600 pt-[5px] text-end'>
+          <span className=' text-[8px]'>Owned by </span>{" "}
+          {seller.slice(0, 5) + "..." + seller.slice(-5)}
+        </p>
+        <p className='text-tiny uppercase font-bold text-indigo-600 pt-[5px] text-end text-[8px]'>
+          Token Id {tokenId}
+        </p>
       </CardHeader>
-      <CardBody className="overflow-visible py-2">
+      <CardBody className='overflow-visible py-2'>
         <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
+          alt='Card background'
+          className='object-cover rounded-xl'
           src={image}
           width={270}
         />
 
-        <div className="flex flex-col justify-center align-middle text-center p-[10px]">
-      <small className="text-default-500 pt-3">Odometer ---- {odometer} Km</small>
-      <small className="text-default-500">Repairs ---- {repairs}</small>
-      <small className="text-default-500">Repairs Date ---- {repairsDate}</small>
-      <h4 className="font-bold text-large p-2 text-indigo-600">{price} SepoliaETH</h4>
-      
+        <div className='flex flex-col justify-center align-middle text-center p-[10px]'>
+          <small className='text-default-500 pt-3'>
+            Odometer ---- {odometer} Km
+          </small>
+          <small className='text-default-500'>Repairs ---- {repairs}</small>
+          <small className='text-default-500'>
+            Repairs Date ---- {repairsDate}
+          </small>
+          <h4 className='font-bold text-large p-2 text-indigo-600'>
+            {price} SepoliaETH
+          </h4>
         </div>
-      <Button as={Link} color="secondary" variant="ghost" href={{
-        pathname: "/seller/listings-seller/offers",
-        query: {
-          nftAddress: nftAddress,
-          tokenId : tokenId,
-        }
-      }}>Check Offers</Button>
-    
-
+        <Button
+          as={Link}
+          color='secondary'
+          variant='ghost'
+          href={{
+            pathname: "/seller/listings-seller/offers",
+            query: {
+              nftAddress: nftAddress,
+              tokenId: tokenId,
+            },
+          }}
+        >
+          Check Offers
+        </Button>
       </CardBody>
-      
     </Card>
   );
 }
