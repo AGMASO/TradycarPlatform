2d6e2bff28dc06aef7653317db8cfb9e2a6e8a70
Alejandro <95312462+AGMASO@users.noreply.github.com>
2024-03-04 17:34:55 +0100

    Update seller-listing.tsx

--- tradify-front/app/ui/seller-listing.tsx
+++ tradify-front/app/ui/seller-listing.tsx
@@ -1,36 +1,44 @@
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Input } from "@nextui-org/react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input,
+} from "@nextui-org/react";
 import { useState } from "react";
 
-import createListing from "../lib/scripts/createListing"
+import createListing from "../lib/scripts/createListing";
 
 export default function SellerListing() {
   const [isOpen, setIsOpen] = useState(false);
   const [formData, setFormData] = useState({
     nftAddress: "",
     tokenId: "",
-    price: ""
+    price: "",
   });
 
-  const handleChange = (e : any) => {
+  const handleChange = (e: any) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
   };
 
-  const handleSubmit = async (e : any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     try {
+      console.log(formData.nftAddress, formData.price, formData.tokenId);
 
-      console.log( formData.nftAddress, formData.price, formData.tokenId);
-
-       
-
-      await createListing(formData.nftAddress, formData.tokenId, formData.price);
+      await createListing(
+        formData.nftAddress,
+        formData.tokenId,
+        formData.price
+      );
 
       console.log("Success to create Listing");
-      
     } catch (error) {
       console.error("Error submitting form:", error);
     }
@@ -43,65 +51,61 @@ export default function SellerListing() {
     setFormData({
       nftAddress: "",
       tokenId: "",
-      price: ""
+      price: "",
     });
     setIsOpen(false);
   };
 
   return (
     <>
-      <Button onPress={() => setIsOpen(true)} color="secondary" variant="ghost">
+      <Button onPress={() => setIsOpen(true)} color='secondary' variant='ghost'>
         Create Listing
       </Button>
-      <Modal
-        isOpen={isOpen}
-        onClose={handleCloseModal}
-        placement="top-center"
-      >
+      <Modal isOpen={isOpen} onClose={handleCloseModal} placement='top-center'>
         <ModalContent>
           <form onSubmit={handleSubmit}>
-            <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+            <ModalHeader className='flex flex-col gap-1'>Log in</ModalHeader>
             <ModalBody>
               <Input
                 autoFocus
-                label="Nft Address"
-                type="text"
-                id="nftAddress"
-                name="nftAddress"
-                placeholder="Enter the address of your NFT"
-                variant="bordered"
+                label='Nft Address'
+                type='text'
+                id='nftAddress'
+                name='nftAddress'
+                placeholder='Enter the address of your NFT'
+                variant='bordered'
                 value={formData.nftAddress}
                 onChange={handleChange}
-                className=" text-indigo-600"
+                className=' text-indigo-600'
               />
               <Input
-                label="Token Id"
-                placeholder="Enter the token Id of your NFT"
-                type="text"
-                id="tokenId"
-                name="tokenId"
+                label='Token Id'
+                placeholder='Enter the token Id of your NFT'
+                type='text'
+                id='tokenId'
+                name='tokenId'
                 value={formData.tokenId}
                 onChange={handleChange}
-                variant="bordered"
-                className=" text-indigo-600"
+                variant='bordered'
+                className=' text-indigo-600'
               />
               <Input
-                label="Price"
-                placeholder="Enter the desired price for sell"
-                type="text"
-                id="price"
-                name="price"
+                label='Price'
+                placeholder='Enter the desired price for sell'
+                type='text'
+                id='price'
+                name='price'
                 value={formData.price}
                 onChange={handleChange}
-                variant="bordered"
-                className=" text-indigo-600"
+                variant='bordered'
+                className=' text-indigo-600'
               />
             </ModalBody>
             <ModalFooter>
-              <Button color="danger" variant="flat" onClick={handleCloseModal}>
+              <Button color='danger' variant='flat' onClick={handleCloseModal}>
                 Close
               </Button>
-              <Button type="submit" color="primary">
+              <Button type='submit' color='primary'>
                 Create Listing
               </Button>
             </ModalFooter>
